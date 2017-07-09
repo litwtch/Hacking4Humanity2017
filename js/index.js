bc2cb4c3122a2c2c73b474851c4d681a735b4a3d
Maribel Duran <maribelduranucm@gmail.com>
2017-07-09 12:48:49 -0700

    More commits

--- js/index.js
+++ js/index.js
@@ -17,12 +17,12 @@ function shelter(name, wishlist, needs, website){
 
 shelterApp.prototype.addShelter = function(name, wishlist, needs, website){
   var s = new shelter(name, wishlist, needs, website);
-	console.log(s);
+  console.log(s);
   this.shelters.push(s);
 }
 
 shelterList.prototype.addShelter = function(name, wishlist, needs, website){
-	this.shelters.push({
+  this.shelters.push({
       name: name,
       wishlist: wishlist,
       needs:needs,
@@ -31,17 +31,17 @@ shelterList.prototype.addShelter = function(name, wishlist, needs, website){
 }
 
 shelterApp.prototype.addNeeds = function(list){
-	list.forEach(function(need){
-			if (this.needs.indexOf(need) == -1){
-				this.needs.push(need);
-			}
-	}.bind(this));
+  list.forEach(function(need){
+      if (this.needs.indexOf(need) == -1){
+        this.needs.push(need);
+      }
+  }.bind(this));
 }
 
 var model = {
   app: {},
   initApp: function(){
-  	this.app = new shelterApp();
+    this.app = new shelterApp();
 
   }
 };
@@ -53,22 +53,22 @@ var findOne = function (haystack, arr) {
 };
 
 var controller = {
-	initApp: function(){
-		model.initApp();
-		//this.getNeeds();
-		//this.getShelters(["socks", "baby wipes"]);
-	},
-	getNeeds: function(){
-		//Update url to our github repository for this project
-		var url = 'http://www.maribelduran.com/twitch_streamers/js/ShelterDictionary.JSON';
+  initApp: function(){
+    model.initApp();
+    //this.getNeeds();
+    //this.getShelters(["socks", "baby wipes"]);
+  },
+  getNeeds: function(){
+    //Update url to our github repository for this project
+    var url = 'http://www.maribelduran.com/twitch_streamers/js/ShelterDictionary.JSON';
     var request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.onload = function() {
     if (request.status >= 200 && request.status < 400) { 
       var shelters = JSON.parse(request.response);
-			shelters.forEach(function(obj){
-			var needs= obj["needs"];
-			model.app.addNeeds(needs);
+      shelters.forEach(function(obj){
+      var needs= obj["needs"];
+      model.app.addNeeds(needs);
       });
       console.log(model.app.needs);
       //userList.sortUsers();
@@ -81,39 +81,39 @@ var controller = {
       // There was a connection error of some sort
     };
     request.send();
-	},
+  },
 
-	retrieveShelters: function(needs){
-		var list = new shelterList();
-		 var requests = [];
-	requests.push(this.getShelters(needs).then(function(response){
+  retrieveShelters: function(needs){
+    var list = new shelterList();
+     var requests = [];
+  requests.push(this.getShelters(needs).then(function(response){
       var shelters = JSON.parse(response);
       shelters.forEach(function(obj){
-				//check to see if there is at least one matching need 
-				if (findOne(obj.needs, needs)){
-					//finds the interescting needs
-					var commonNeeds = needs.filter(function(n){
-    					return obj.needs.indexOf(n) !== -1;
-					});
-					//model.app.addShelter(obj.shelter, obj.wishList, commonNeeds, obj.website);
-					list.addShelter(obj.shelter, obj.wishList, commonNeeds, obj.website);
-				}
-			});
+        //check to see if there is at least one matching need 
+        if (findOne(obj.needs, needs)){
+          //finds the interescting needs
+          var commonNeeds = needs.filter(function(n){
+              return obj.needs.indexOf(n) !== -1;
+          });
+          //model.app.addShelter(obj.shelter, obj.wishList, commonNeeds, obj.website);
+          list.addShelter(obj.shelter, obj.wishList, commonNeeds, obj.website);
+        }
+      });
     },function(error) {
         console.error("Failed!", error);
     }))
 
-	Promise.all(requests).then(function(results) {
-  	this.postShelters(list.shelters);
+  Promise.all(requests).then(function(results) {
+    this.postShelters(list.shelters);
   }.bind(this)).catch(function(error) {
   // One or more promises was rejected
   }.bind(this));
-	
-	},
-	getShelters: function(needs){
-		return new Promise(function(resolve,reject){
-		//Update url to our github repository for this project
-		var url = 'http://www.maribelduran.com/testing/js/ShelterDictionary.JSON';
+  
+  },
+  getShelters: function(needs){
+    return new Promise(function(resolve,reject){
+    //Update url to our github repository for this project
+    var url = 'http://www.maribelduran.com/testing/js/ShelterDictionary.JSON';
     var request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.onload = function() {
@@ -133,14 +133,14 @@ var controller = {
  });
 },
 postShelters: function(shelter){
-
+  console.log(shelter);
   document.getElementById("msg").innerHTML = shelter;
-	/*$.ajax({
-		type : "post",
-		url : "results.html",
-		data : shelter,
-		cache : false,
-		success : function(html)
+  /*$.ajax({
+    type : "post",
+    url : "results.html",
+    data : shelter,
+    cache : false,
+    success : function(html)
           { alert(html);
           $('#msg').html(html);
      }});
@@ -151,33 +151,33 @@ postShelters: function(shelter){
 function getUrlVars()
 {
     var vars = [], hash;
-    
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
 
     for(var i = 0; i < hashes.length; i++)
         {
          hash = hashes[i].split('=');
          vars.push(hash[0]);
-         vars[hash[0]] = hash[1];
+         //vars[hash[0]] = hash[1];
          }
 
      return vars;
 }
 
-var get = getUrlVars();
-console.log(get);
+var needs = getUrlVars();
+controller.retrieveShelters(needs);
+
 
 var view = {
   setUpEventListeners: function(){
-  	//set up an event listener for a submit button or click button that will give us the needs list
-		document.getElementById("submitNeeds").addEventListener("click", function(){
+    //set up an event listener for a submit button or click button that will give us the needs list
+    document.getElementById("submitNeeds").addEventListener("click", function(){
       
     //var url = 'http://www.maribelduran.com/testing/js/ShelterDictionary.JSON';
     //window.location = 
-			var needs = ["socks", "baby wipes"];
-			controller.retrieveShelters(needs);
-		 });
-	}
+      var needs = ["socks", "baby wipes"];
+      controller.retrieveShelters(needs);
+     });
+  }
 };
 
 controller.initApp();
